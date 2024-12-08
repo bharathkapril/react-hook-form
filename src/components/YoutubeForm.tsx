9d369ce336daff5b18be49bd7777f782d2c68415
bharathkapril <bharathapril25@gmail.com>
2024-12-08 14:17:43 +0530

    React Hook Form Tutorial - 28 - Manually Trigger Validations

--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -40,6 +40,7 @@ const YoutubeForm = () => {
     getValues,
     setValue,
     reset,
+    trigger,
   } = useForm<FormValues>({
     defaultValues: {
       username: "",
@@ -247,34 +248,44 @@ const YoutubeForm = () => {
           })}
         />
         <p className="text-red-400">{errors.dob?.message}</p>
-        <button
-          disabled={!isDirty || !isValid || isSubmitting}
-          className="bg-green-600 flex px-1 py-2 rounded flex-[1_0_0] disabled:opacity-50 disabled:cursor-not-allowed"
-        >
-          Submit
-        </button>
-        <button
-          type="button"
-          onClick={() => reset()}
-          // disabled={!isDirty}
-          className="bg-amber-800 flex px-1 py-2 rounded flex-[1_0_0] disabled:opacity-50 disabled:cursor-not-allowed"
-        >
-          Reset
-        </button>
-        <button
-          type="button"
-          onClick={handleGetValues}
-          className="bg-orange-600 flex px-1 py-2 rounded flex-[1_0_0]"
-        >
-          Get Values
-        </button>
-        <button
-          type="button"
-          onClick={handleSetValue}
-          className="bg-lime-500 flex px-1 py-2 rounded flex-[1_0_0]"
-        >
-          Set Value
-        </button>
+        <div className="flex gap-2">
+          <button
+            disabled={!isDirty || !isValid || isSubmitting}
+            className="bg-green-600 flex px-1 py-2 rounded flex-[1_0_0] disabled:opacity-50 disabled:cursor-not-allowed"
+          >
+            Submit
+          </button>
+          <button
+            type="button"
+            onClick={() => reset()}
+            // disabled={!isDirty}
+            className="bg-amber-800 flex px-1 py-2 rounded flex-[1_0_0] disabled:opacity-50 disabled:cursor-not-allowed"
+          >
+            Reset
+          </button>
+          <button
+            type="button"
+            onClick={handleGetValues}
+            className="bg-orange-600 flex px-1 py-2 rounded flex-[1_0_0]"
+          >
+            Get Values
+          </button>
+          <button
+            type="button"
+            onClick={handleSetValue}
+            className="bg-lime-500 flex px-1 py-2 rounded flex-[1_0_0]"
+          >
+            Set Value
+          </button>
+
+          <button
+            type="button"
+            onClick={() => trigger("channel")}
+            className="bg-blue-500 flex px-1 py-2 rounded flex-[1_0_0]"
+          >
+            Trigger
+          </button>
+        </div>
       </form>
       <DevTool control={control} />
     </div>
